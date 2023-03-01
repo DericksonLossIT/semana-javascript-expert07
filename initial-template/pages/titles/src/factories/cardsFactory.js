a5f86e82a5a4521bbb6b4424d4c62645c817bf0e
DericksonLossIT <dericksonlossit@gmail.com>
2023-03-01 20:17:17 -0300

    implements cardListWorker

--- initial-template/pages/titles/src/factories/cardsFactory.js
+++ initial-template/pages/titles/src/factories/cardsFactory.js
@@ -1,14 +1,27 @@
-import CardsController from "./../controllers/cardsController.js"
-import CardsView from "./../views/cardsView.js"
-import CardsService from "./../services/cardsService.js"
-const [rootPath] = window.location.href.split('/pages/')
+import CardsController from "./../controllers/cardsController.js";
+import CardsView from "./../views/cardsView.js";
+import CardsService from "./../services/cardsService.js";
+import path from "node:path";
+
+const cardListWorker = new Worker(
+  `${path.resolve(`../workers/cardListWorker.js`)}`,
+  {
+    type: "module",
+  }
+);
+cardListWorker.onmessage = (msg) => {
+  console.log("processo principal", msg);
+};
+cardListWorker.postMessage("Hey hello!");
+const [rootPath] = window.location.href.split("/pages/");
 const factory = {
   async initialize() {
     return CardsController.initialize({
+      worker: cardListWorker,
       view: new CardsView(),
-      service: new CardsService({ dbUrl: `${rootPath}/assets/database.json` })
-    })
-  }
-}
+      service: new CardsService({ dbUrl: `${rootPath}/assets/database.json` }),
+    });
+  },
+};
 
-export default factory
+export default factory;
